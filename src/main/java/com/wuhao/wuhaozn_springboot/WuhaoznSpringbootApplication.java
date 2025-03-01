8893d04ae2b917cfc2013cb8c093a7511b1547df
Shenshuyin <haicongzhi@outlook.com>
2025-03-01 10:42:56 +0800

    1.解决第一次不加载静态资源url错误问题220250301；

--- src/main/java/com/wuhao/wuhaozn_springboot/WuhaoznSpringbootApplication.java
+++ src/main/java/com/wuhao/wuhaozn_springboot/WuhaoznSpringbootApplication.java
@@ -3,15 +3,43 @@ package com.wuhao.wuhaozn_springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
 
 @SpringBootApplication
 @MapperScan({"com.wuhao.wuhaozn_springboot.mapper","com.wuhao.wuhaozn_springboot.protal_mapper"})
-public class WuhaoznSpringbootApplication {
+public class WuhaoznSpringbootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WuhaoznSpringbootApplication.class, args);
     }
 
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//        super.onStartup(servletContext);
+//        servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+//        SessionCookieConfig sessionCookieConfig=servletContext.getSessionCookieConfig();
+//        sessionCookieConfig.setHttpOnly(true);
+//    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(WuhaoznSpringbootApplication.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+        SessionCookieConfig sessionCookieConfig=servletContext.getSessionCookieConfig();
+        sessionCookieConfig.setHttpOnly(true);
+    }
 }
 
 
--- src/main/java/com/wuhao/wuhaozn_springboot/config/InterceptorConfig.java
+++ src/main/java/com/wuhao/wuhaozn_springboot/config/InterceptorConfig.java
@@ -1,9 +1,16 @@
 package com.wuhao.wuhaozn_springboot.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionTrackingMode;
+import java.util.Collections;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
@@ -18,5 +25,16 @@ public class InterceptorConfig implements WebMvcConfigurer {
             .addResourceLocations("file:" + imagePath);
     System.out.println("InterceptorConfig imagePath = " + imagePath);
   }
+
+//  @Bean
+//  public ServletContextInitializer servletContextInitializer1() {
+//    return new ServletContextInitializer() {
+//      @Override
+//      public void onStartup(ServletContext servletContext) throws ServletException {
+//        servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE) );
+//      }
+//    };
+//  }
+
 }
 
